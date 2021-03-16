e6686564a5c86be380b03362def930e43392ea51
garethahealy <garethahealy@gmail.com>
2021-03-16 09:29:30 +0000

    fix to test ci passes

--- _test/bats-support-clone.bash
+++ _test/bats-support-clone.bash
@@ -5,5 +5,5 @@ fi
 
 if [[ ! -d "_test/test_helper/redhatcop-bats-library" ]]; then
   # Download redhat-cop/bats-library dynamically so it doesnt need to be added into source
-  git clone https://github.com/redhat-cop/bats-library _test/test_helper/redhatcop-bats-library --depth 1
+  git clone https://github.com/garethahealy/bats-library.git _test/test_helper/redhatcop-bats-library --depth 1
 fi
